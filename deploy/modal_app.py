f6c429a2a62628145fb873b5afaecd5784bf3e9a
Nik Kim <knocknocknik@gmail.com>
2026-04-26 21:56:14 -0700

    revert generate param

--- deploy/modal_app.py
+++ deploy/modal_app.py
@@ -100,7 +100,10 @@ class Model:
             out = self.model.generate(
                 **inputs,
                 max_new_tokens=600,
-                do_sample=False,  # greedy: deterministic, less hallucination
+                do_sample=True,
+                temperature=0.7,
+                top_p=0.9,
+                repetition_penalty=1.3,  # strong penalty to prevent collapse loops
             )
 
         new_tokens = out[0][inputs["input_ids"].shape[1]:]
